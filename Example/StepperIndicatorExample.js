3350671ce14ef9c53b362c4ea63535785eb8878a
Halil Bilir <hbilir@gmail.com>
2016-12-22 12:22:12 +0300

    initial commit

---
+++ Example/StepperIndicatorExample.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+} from 'react-native';
+
+import StepperIndicator from 'react-native-stepper-indicator';
+
+export default class StepperIndicatorExample extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StepperIndicator />
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingLeft: 20,
+  },
+});
---
+++ Example/index.ios.js
@@ -0,0 +1,7 @@
+import {
+  AppRegistry,
+} from 'react-native';
+
+import StepperIndicatorExample from './StepperIndicatorExample';
+
+AppRegistry.registerComponent('StepperIndicatorExample', () => StepperIndicatorExample);
